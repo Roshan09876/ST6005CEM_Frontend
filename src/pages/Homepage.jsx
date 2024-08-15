f5353016cf9d557c6bd316f6a067ada164e3eaf3
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2024-08-15 21:30:31 +0545

    home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,19 +3,9 @@ import Hero from "../components/Hero";
 import Service from "../components/Service";
 import TechWeLove from "../components/TechWeLove";
 import TrustedBy from "../components/TrustedBy";
-// import { testApi } from "../api/Api";
 import Footer from "../components/Footer"
 
 const Homepage = () => {
-  // useEffect(() => {
-  //   testApi()
-  //     .then(response => {
-  //       console.log(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
 
   return (
     <div className="mt-5">
